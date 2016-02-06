b70d115ec0ea4837dfa036b21a949a92bb725719
Justin Dearing <zippy1981@gmail.com>
2016-02-05 22:28:21 -0500

    Sample App with SSNs

---
+++ AlwayEncryptedSample/Migrations/Configuration.cs
@@ -0,0 +1,33 @@
+namespace AlwayEncryptedSample.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<AlwayEncryptedSample.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
+            ContextKey = "AlwayEncryptedSample.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(AlwayEncryptedSample.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
---
+++ AlwayEncryptedSample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AlwayEncryptedSample.Startup))]
+namespace AlwayEncryptedSample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
