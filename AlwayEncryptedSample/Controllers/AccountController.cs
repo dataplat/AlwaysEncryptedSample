cc79d3b5f04ced529fd21ae4f2e14417efafbeaf
Justin Dearing <zippy1981@gmail.com>
2016-02-05 23:42:36 -0500

    WOrking with encryption

--- AlwayEncryptedSample/Controllers/AccountController.cs
+++ AlwayEncryptedSample/Controllers/AccountController.cs
@@ -151,7 +151,7 @@ namespace AlwayEncryptedSample.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email, SSN = model.SSN };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
