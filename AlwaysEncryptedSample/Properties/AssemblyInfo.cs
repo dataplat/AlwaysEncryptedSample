5fd17be8d88a74fc4c14eec42555dbe9b5df8677
Justin Dearing <zippy1981@gmail.com>
2019-05-06 10:11:01 -0400

    GlobalAssemblyInfo.cs. Use nuget variables.

--- AlwaysEncryptedSample/Properties/AssemblyInfo.cs
+++ AlwaysEncryptedSample/Properties/AssemblyInfo.cs
@@ -7,32 +7,9 @@ using log4net.Config;
 // associated with an assembly.
 [assembly: AssemblyTitle("AlwaysEncryptedSample")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AlwaysEncryptedSample")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("56c34e4d-7d95-46eb-8348-343b842dd45e")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 
 // Log4Net configuration. 
 [assembly:XmlConfigurator()]
