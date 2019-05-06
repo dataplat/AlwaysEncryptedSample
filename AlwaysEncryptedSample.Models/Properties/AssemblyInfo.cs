d1ea22591e289f52397b09f08bd69b4c12d702e4
Justin Dearing <zippy1981@gmail.com>
2019-05-06 13:31:01 -0400

    Added a readme.

--- AlwaysEncryptedSample.Models/Properties/AssemblyInfo.cs
+++ AlwaysEncryptedSample.Models/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AlwaysEncryptedSample.Models")]
-[assembly: AssemblyDescription("EF Core 6 and Asp.Net Identity Object models")]
+[assembly: AssemblyDescription("EF Core 6 and Asp.Net Identity Object models for the AlwaysEncryptedSample. ")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("6f9599eb-3c9a-45ad-a081-30f83946d63c")]
---
+++ AlwaysEncryptedSample.Models/Readme.md
@@ -0,0 +1,5 @@
+# AlwaysEncryptedSample.Models
+
+[![NuGet](http://img.shields.io/nuget/v/AlwaysEncryptedSample.Models.svg)](https://www.nuget.org/packages/AlwaysEncryptedSample.Models)
+
+EF Core 6 and Asp.Net Identity Object models for the AlwaysEncryptedSample.
