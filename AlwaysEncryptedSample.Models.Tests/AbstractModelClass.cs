b0c177e7398c5f30aad144fab63e8e8e98d8ac0f
Justin Dearing <zippy1981@gmail.com>
2019-05-07 00:39:14 -0400

    Added unit tests for models

---
+++ AlwaysEncryptedSample.Models.Tests/AbstractModelClass.cs
@@ -0,0 +1,19 @@
+﻿// NUnit 3 tests
+// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using System.Data.SqlClient;
+using NUnit.Framework;
+
+namespace AlwaysEncryptedSample.Models
+{
+    public abstract class AbstractModelClass
+    {
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            DbInit.CreateDatabase(new SqlConnectionStringBuilder());
+            DbInit.CreateAuthContext();
+            DbInit.CreatePurchasingContext();
+            DbInit.InitLog4NetDb(new SqlConnection());
+        }
+    }
+}
