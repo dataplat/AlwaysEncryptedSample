2bb81c2af62408ba9901ea0bc7402806b7a9cbb6
Justin Dearing <zippy1981@gmail.com>
2019-05-16 14:27:14 -0400

    Clean up powershell formatting. (#31)

--- New-EncryptionKeys.ps1
+++ New-EncryptionKeys.ps1
@@ -4,87 +4,84 @@
 
 [cmdletbinding()]
 param(
-	[Parameter(Mandatory = $true, ValueFromPipeline = $true)] [string] $ConnectionString,
-	[string] $MasterKeyDNSName = "CN=Always Encrypted Sample Cert",
-	[switch] $RemoveExistingCerts,
-	[switch] $ExportCertificate,
-	[switch] $ExportCertificateKeys,
-	[string] $MasterKeySQLName = "AlwaysEncryptedSampleCMK",
-	[string] $AuthColumnKeyName = "AuthColumnsKey",
-	[string] $AppColumnKeyName = "AppColumnsKey",
-	[string] $LogColumnKeyName = "LogColumnsKey"
+    [Parameter(Mandatory = $true, ValueFromPipeline = $true)] [string] $ConnectionString,
+    [string] $MasterKeyDNSName = "CN=Always Encrypted Sample Cert",
+    [switch] $RemoveExistingCerts,
+    [switch] $ExportCertificate,
+    [switch] $ExportCertificateKeys,
+    [string] $MasterKeySQLName = "AlwaysEncryptedSampleCMK",
+    [string] $AuthColumnKeyName = "AuthColumnsKey",
+    [string] $AppColumnKeyName = "AppColumnsKey",
+    [string] $LogColumnKeyName = "LogColumnsKey"
 )
 
-Import-Module SqlServer
-
 try {
-	$smoDatabase = Get-SqlDatabase -ConnectionString $ConnectionString
+    $smoDatabase = Get-SqlDatabase -ConnectionString $ConnectionString
 }
 catch {
-	Write-Error $_
-	break
+    Write-Error $_
+    break
 }
 
 if ($RemoveExistingCerts) {
-	Write-Verbose "Removing All Existing Certificates Named $($MasterKeyDNSName)"
+    Write-Verbose "Removing All Existing Certificates Named $($MasterKeyDNSName)"
     $existingColumns = Get-SqlColumnEncryptionKey -InputObject $smoDatabase
     $existingColumns | ForEach-Object {
         Remove-SqlColumnEncryptionKey -Name $_.Name -InputObject $smoDatabase
     }
     Remove-SqlColumnMasterKey -Name $MasterKeySQLName -InputObject $smoDatabase
-	Get-ChildItem Cert:\CurrentUser\My |  Where-Object subject -eq $MasterKeyDNSName | Remove-Item
+    Get-ChildItem Cert:\CurrentUser\My | Where-Object subject -eq $MasterKeyDNSName | Remove-Item
 }
 
-$Cert = (Get-ChildItem Cert:\CurrentUser\My |  Where-Object subject -eq 'CN=Always Encrypted Sample Cert') | Select-Object Thumbprint -First 1
+$Cert = (Get-ChildItem Cert:\CurrentUser\My | Where-Object subject -eq 'CN=Always Encrypted Sample Cert') | Select-Object Thumbprint -First 1
 if ($Cert) {
-	Write-Verbose "Certificate `"$($MasterKeyDNSName)`" Already exists"
+    Write-Verbose "Certificate `"$($MasterKeyDNSName)`" Already exists"
 }
 else {
-	Write-Host "Creating Self Signed Certificate `"$($MasterKeyDNSName)`""
-	$Cert = New-SelfSignedCertificate `
-		-Subject $MasterKeyDNSName `
-		-CertStoreLocation Cert:\CurrentUser\My `
-		-KeyExportPolicy Exportable `
-		-Type DocumentEncryptionCert `
-		-KeyUsage DataEncipherment `
-		-KeySpec KeyExchange
-	 = "CurrentUser/My/$($cert.ThumbPrint)"
-	Write-Verbose "Certificate Master Key Path: $($cmkPath)"
+    Write-Host "Creating Self Signed Certificate `"$($MasterKeyDNSName)`""
+    $Cert = New-SelfSignedCertificate `
+        -Subject $MasterKeyDNSName `
+        -CertStoreLocation Cert:\CurrentUser\My `
+        -KeyExportPolicy Exportable `
+        -Type DocumentEncryptionCert `
+        -KeyUsage DataEncipherment `
+        -KeySpec KeyExchange
+    $CmkPath = "Cert:\CurrentUser\My\$($cert.ThumbPrint)"
+    Write-Verbose "Column Master Key Certificate Path: $($CmkPath)"
 }
 
 if ($ExportCertificate) {
-	Get-ChildItem Cert:\CurrentUser\My | 
-		Where-Object subject -eq "CN=Always Encrypted Sample Cert" | 
-		Export-Certificate -FilePath "$($MasterKeySQLName).cer" | Out-Null
+    Get-ChildItem Cert:\CurrentUser\My |
+    Where-Object subject -eq "CN=Always Encrypted Sample Cert" |
+    Export-Certificate -FilePath "$($MasterKeySQLName).cer" | Out-Null
 }
 
 if ($ExportCertificateKeys) {
-	Get-ChildItem Cert:\CurrentUser\My | 
-		Where-Object subject -eq "CN=Always Encrypted Sample Cert" | 
-		Export-PfxCertificate -FilePath "$($MasterKeySQLName).pfx" -Password (ConvertTo-SecureString -String "1234" -Force -AsPlainText) | Out-Null
+    Get-ChildItem Cert:\CurrentUser\My |
+    Where-Object subject -eq "CN=Always Encrypted Sample Cert" |
+    Export-PfxCertificate -FilePath "$($MasterKeySQLName).pfx" -Password (ConvertTo-SecureString -String "1234" -Force -AsPlainText) | Out-Null
 }
 
-if($smoDatabase.ColumnMasterKeys['AlwaysEncryptedSampleCMK']) {
-	Write-Warning "Master Key Reference $($MasterKeySQLName) already exists in the database."
+if ($smoDatabase.ColumnMasterKeys['AlwaysEncryptedSampleCMK']) {
+    Write-Warning "Master Key Reference $($MasterKeySQLName) already exists in the database."
 }
 else {
-	# Create a SqlColumnMasterKeySettings object for your column master key. 
-	$cmkSettings = New-SqlCertificateStoreColumnMasterKeySettings `
-		-CertificateStoreLocation "CurrentUser" `
-		-Thumbprint $Cert.Thumbprint
-		
-	New-SqlColumnMasterKey -Name $MasterKeySQLName -InputObject $smoDatabase -ColumnMasterKeySettings $cmkSettings | Out-Null
+    # Create a SqlColumnMasterKeySettings object for your column master key.
+    $cmkSettings = New-SqlCertificateStoreColumnMasterKeySettings `
+        -CertificateStoreLocation "CurrentUser" `
+        -Thumbprint $Cert.Thumbprint
+
+    New-SqlColumnMasterKey -Name $MasterKeySQLName -InputObject $smoDatabase -ColumnMasterKeySettings $cmkSettings | Out-Null
 }
 
-$ExistingColumnKeys = $smoDatabase.ColumnEncryptionKeys 
+$ExistingColumnKeys = $smoDatabase.ColumnEncryptionKeys
 @($AuthColumnKeyName, $AppColumnKeyName, $LogColumnKeyName) | ForEach-Object {
-	if ($ExistingColumnKeys[$_]) {
-		Write-Warning "Column Encryption Key already $_ exists."
-	}
-	else {
-		$smoDatabase | New-SqlColumnEncryptionKey `
-			-ColumnMasterKey $MasterKeySQLName `
-			-Name $_ | Out-Null
-	}
+    if ($ExistingColumnKeys[$_]) {
+        Write-Warning "Column Encryption Key already $_ exists."
+    }
+    else {
+        $smoDatabase | New-SqlColumnEncryptionKey `
+            -ColumnMasterKey $MasterKeySQLName `
+            -Name $_ | Out-Null
+    }
 }
-	
